2ee6858bff7825ed6d52f9ef89895663745ced12
Miro Pöllänen <mir0pollan3njaa@gmail.com>
2017-12-14 13:27:23 +0200

    Add files via upload

---
+++ palautukset/demo_db_connection.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+host: "magnesium",
+user: "a16mpollanen",
+database: "ska16mpollanen",
+password: "salasana"
+});
+
+con.connect(function(err) {
+if (err) throw err;
+console.log("Yhdistetty!");
+
+});
